63849e72a5c565d353954977406e3a84c7b26528
mintaeKim <peterzzo@naver.com>
2024-03-14 17:01:26 +0900

    240314 crypto home end

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,12 +5,10 @@ import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Coins/>}>
-        </Route>
-        <Route path="/:coinId" element={<Coin/>}>
-        </Route>
+        <Route path="/" element={<Coins/>} />
+        <Route path="/:coinId" element={<Coin/>} />
       </Routes>
     </BrowserRouter>
   );
